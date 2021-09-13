7945ac889cca179fb8be0d171d0aa9b0d77c2e60
Alao-Abiodun <46402042+Alao-Abiodun@users.noreply.github.com>
2021-09-13 15:43:12 +0100

    refractor/change the DS logic to use the mysql logic persistence

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -1,36 +1,67 @@
-const {users} = require('../utils/dummyData');
-const User = require('../models/user.model');
-const customError = require('../utils/customError');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const { users } = require("../utils/dummyData");
+const User = require("../models/user.model");
+const db = require("../databases/db");
+const customError = require("../utils/customError");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-const {ACCESS_TOKEN_SECRET, REFREHS_TOKEN_SECRET} = process.env;
+const { ACCESS_TOKEN_SECRET, REFREHS_TOKEN_SECRET } = process.env;
 
 // console.log(users);
 
 const userServices = {
-    fetchUsers (req, res) {
-        const validUsers = users.map(user => user)
-        return validUsers;
-    },
+  async fetchUsers(req, res) {
+    const users = await db.execute("SELECT * FROM users");
+    const validUsers = await users[0].map((user) => user);
+    return validUsers;
+  },
 
-    signUp (user) {
-        const userLength = users.length;
-        const lastId = users[userLength - 1].id;
-        const newId = lastId + 1;
-        user.id = newId;
-        users.push(user);
-        return user;
-    },
-
-    login (email) {
-        const foundUser = users.find(user => user.email == email);
-        if (!foundUser) {
-            return new customError(401, 'user not found!');
-        }
-        const accessToken = jwt.sign(foundUser.email, ACCESS_TOKEN_SECRET);
-        return {foundUser, token: accessToken} || {};
+  async signUp(user) {
+    let userDetails = {
+      firstname: user.firstname,
+      lastname: user.lastname,
+      othernames: user.othernames,
+      email: user.email,
+      phonenumber: user.phonenumber,
+      username: user.username,
+      registered: user.registered,
+      isAdmin: user.isAdmin,
+    };
+    try {
+      const newUser = await db.execute(
+        "INSERT INTO users (firstName, lastName, othernames, email, phonenumber, username, registered, isAdmin) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+        [
+          userDetails.firstname,
+          userDetails.lastname,
+          userDetails.othernames,
+          userDetails.email,
+          userDetails.phonenumber,
+          userDetails.username,
+          userDetails.registered,
+          userDetails.isAdmin,
+        ]
+      );
+      return newUser;
+    } catch (error) {
+      console.log(error);
+      return new customError(500, "Server Error");
     }
-}
+  },
+
+  async login(email) {
+    try {
+      const users = await db.execute("SELECT * FROM users");
+      const foundUser = users[0].find((user) => user.email == email);
+      if (!foundUser) {
+        return new customError(401, "user not found!");
+      }
+      const accessToken = jwt.sign(foundUser.email, ACCESS_TOKEN_SECRET);
+      return { foundUser, token: accessToken } || {};
+    } catch (error) {
+      console.log(error);
+      return new customError(500, "Server Error");
+    }
+  },
+};
 
 module.exports = userServices;
